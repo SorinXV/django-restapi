76b54bc0b54fcc3399bb9364f1ad68c74063a3c4
Sorinxv <sorinmartinescu06@gmail.com>
2023-05-17 17:45:06 +0200

    working post method

--- api/views.py
+++ api/views.py
@@ -10,3 +10,9 @@ def getData(request):
     # many is saying we're serializing multiple items, we have 4 in the database
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data = request.data)
+    serializer.save() if serializer.is_valid() else "error"
+    return Response(serializer.data)
